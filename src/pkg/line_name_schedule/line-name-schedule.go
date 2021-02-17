dc35cecea5547d021ab97e466d087e6e8f6d3015
Andrés de la Torre <delatorre.an@gmail.com>
2021-02-17 16:06:32 -0500

    feat: Create a Line type

---
+++ src/pkg/line_name_schedule/line-name-schedule.go
@@ -0,0 +1,7 @@
+package line_name_schedule
+
+// Line type provides a structure that takes care of getting Name and Schedule that constitute a line
+type Line struct {
+	Name 	 string
+	Schedule []string
+}
