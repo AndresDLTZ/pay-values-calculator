9ed749f5dead9f3faa130486755836a059545118
Andrés de la Torre <delatorre.an@gmail.com>
2021-02-17 21:01:46 -0500

    refactor: Get file path by console input

--- src/main.go
+++ src/main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
-	filePath := "files/name-and-schedules2.txt"
+	var filePath string
+	fmt.Print("Ingrese ruta del archivo con nombre y horarios (.txt): ")
+	// e.g files/name-and-schedules1.txt, files/name-and-schedules2.txt
+	fmt.Scan(&filePath)
+
 	// Define a file facade to then read and validate it, in order to calculate the values to be paid.
 	f, err := file_facade.NewFileFacade(&filePath)
 	if !(err != nil) {
