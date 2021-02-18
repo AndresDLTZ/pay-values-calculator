92d66063dc3500361cf57f3429e5ef51dc3a97e5
Andrés de la Torre <delatorre.an@gmail.com>
2021-02-17 19:09:53 -0500

    feat: Create a function to get duration of valid hour range
    A valid time range is if it is in acme daily payment rate data.

--- src/pkg/tools/time-tools.go
+++ src/pkg/tools/time-tools.go
@@ -1,6 +1,23 @@
 package tools
 
-import "time"
+import (
+	"github.com/andresdltz/pay-values-calculator/src/pkg/payment_rate"
+	"strings"
+	"time"
+)
+
+// GetDurationOfValidHourRange will retrieve a hour range and payment rate,
+// check what schedule it is and return to the hour difference
+func GetDurationOfValidHourRange(hrRange string, sr *payment_rate.ScheduleRate) float64 {
+	hrArr := strings.Split(hrRange, "-")
+	hr1 := ParseStringToTime(&hrArr[0])
+	hr2 := ParseStringToTime(&hrArr[1])
+
+	if !(hr1.Before(ParseStringToTime(&sr.StartTime)) || hr2.After(ParseStringToTime(&sr.EndTime))) {
+		return diffHourTime(&hr1, &hr2)
+	}
+	return -1
+}
 
 // ParseStringToTime will retrieve a string and return a time
 func ParseStringToTime(timeStr *string) time.Time {
